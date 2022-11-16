872c00ebecbb1f868ba136bbca8597fe988b9b46
ManOnTopain <116823830+ManOnTopain@users.noreply.github.com>
2022-11-15 20:06:32 -0400

    Added notify and stuff
    Function notify and updated buttons

--- MainScript.lua
+++ MainScript.lua
@@ -1,81 +1,70 @@
-
-
 _G.Main = {}
 function _G.Main:New(Title)
-
-	local UILibraryTemplate = Instance.new("ScreenGui")
+	
+	local uilibrary_template = Instance.new("ScreenGui")
+	uilibrary_template.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	uilibrary_template.Name = "UILibraryTemplate"
+	uilibrary_template.Parent = game.CoreGui
+	
 	local Frame = Instance.new("Frame")
-	local Part = Instance.new("Frame")
-	local UICorner = Instance.new("UICorner")
-	local UIListLayout = Instance.new("UIListLayout")
-	local Button = Instance.new("TextButton")
-	local UICorner_2 = Instance.new("UICorner")
-	local TextLabel = Instance.new("TextLabel")
-	local UICorner_3 = Instance.new("UICorner")
-	local X = Instance.new("TextButton")
-
-	--Properties:
-
-	UILibraryTemplate.Name = "UILibraryTemplate"
-	UILibraryTemplate.Parent = game.CoreGui
-	UILibraryTemplate.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-	Frame.Parent = UILibraryTemplate
-	Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	Frame.BackgroundColor3 = Color3.new(0, 0, 0)
 	Frame.Position = UDim2.new(0.29178825, 0, 0.0526315793, 0)
 	Frame.Size = UDim2.new(0.471299082, 0, 0.684210539, 0)
+	Frame.Name = "Main"
+	Frame.Parent = uilibrary_template
+	
+	local main_label = Instance.new("TextLabel")
+	main_label.Font = Enum.Font.Unknown
+	main_label.Text = Title
+	main_label.TextColor3 = Color3.new(0, 0, 0)
+	main_label.TextScaled = true
+	main_label.TextSize = 14
+	main_label.TextWrapped = true
+	main_label.BackgroundColor3 = Color3.new(0.34902, 0.34902, 0.34902)
+	main_label.Position = UDim2.new(0.275641024, 0, 0.0246153846, 0)
+	main_label.Size = UDim2.new(0.427350432, 0, 0.0584615394, 0)
+	main_label.Name = "MainLabel"
+	main_label.Parent = Frame
+	
+	local uicorner_3 = Instance.new("UICorner")
+	uicorner_3.Parent = main_label
 
-	Part.Name = "Part"
-	Part.Parent = Frame
-	Part.BackgroundColor3 = Color3.fromRGB(120, 120, 120)
-	Part.Position = UDim2.new(0.0724300817, 0, 0.107692309, 0)
-	Part.Size = UDim2.new(0.864649296, 0, 0.80177021, 0)
-
-	UICorner.Parent = Part
-
-	UIListLayout.Parent = Part
-	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-
-	Button.Name = "Button"
-	Button.Parent = Part
-	Button.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
-	Button.Position = UDim2.new(0, 0, 1.46395189e-08, 0)
-	Button.Size = UDim2.new(0.139093146, 0, 0.228410542, 0)
-	Button.Font = Enum.Font.SourceSansBold
-	Button.Text = "NAME"
-	Button.TextColor3 = Color3.fromRGB(255, 255, 255)
-	Button.TextScaled = true
-	Button.TextSize = 14.000
-	Button.TextWrapped = true
-
-
-	UICorner_2.Parent = Frame
-
-	TextLabel.Parent = Frame
-	TextLabel.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
-	TextLabel.Position = UDim2.new(0.275641024, 0, 0.0246153846, 0)
-	TextLabel.Size = UDim2.new(0.427350432, 0, 0.0584615394, 0)
-	TextLabel.Font = Enum.Font.Unknown
-	TextLabel.Text = "TEXT"
-	TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-	TextLabel.TextScaled = true
-	TextLabel.TextSize = 14.000
-	TextLabel.TextWrapped = true
-
-	UICorner_3.Parent = TextLabel
-
-	X.Name = "X"
-	X.Parent = Frame
-	X.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	X.BackgroundTransparency = 1.000
-	X.Position = UDim2.new(0.908119619, 0, 0.0123076923, 0)
-	X.Size = UDim2.new(0, 31, 0, 27)
+	local X = Instance.new("TextButton")
 	X.Font = Enum.Font.SourceSans
 	X.Text = "X"
-	X.TextColor3 = Color3.fromRGB(255, 255, 255)
-	X.TextSize = 14.000
+	X.TextColor3 = Color3.new(1, 1, 1)
+	X.TextSize = 14
+	X.BackgroundColor3 = Color3.new(1, 1, 1)
+	X.BackgroundTransparency = 1
+	X.Position = UDim2.new(0.908119619, 0, 0.0123076923, 0)
+	X.Size = UDim2.new(0, 31, 0, 27)
+	X.Name = "X"
+	X.Parent = Frame
 
-	-- Scripts:
+
+
+
+	local part = Instance.new("Frame")
+	part.BackgroundColor3 = Color3.new(0.470588, 0.470588, 0.470588)
+	part.Position = UDim2.new(0.0724300817, 0, 0.107692309, 0)
+	part.Size = UDim2.new(0.864649296, 0, 0.80177021, 0)
+	part.Name = "Part"
+	part.Parent = Frame
+
+	local uicorner = Instance.new("UICorner")
+	uicorner.Parent = part
+
+	local uilist_layout = Instance.new("UIListLayout")
+	uilist_layout.SortOrder = Enum.SortOrder.LayoutOrder
+	uilist_layout.Parent = part
+
+
+
+	local uicorner_2 = Instance.new("UICorner")
+	uicorner_2.Parent = Frame
+
+
+
 
 
 	local function FHLVXOL_fake_script() -- X.LocalScript 
@@ -98,7 +87,7 @@ function _G.Main:New(Title)
 	_G.Frame = {}
 	function _G.Frame:Button(Name,Call)
 		local TextButton = Instance.new("TextButton")
-		TextButton.Parent = Part
+		TextButton.Parent = part
 		TextButton.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
 		TextButton.Position = UDim2.new(0, 0, 1.46395189e-08, 0)
 		TextButton.Size = UDim2.new(0.139093146, 0, 0.228410542, 0)
@@ -108,15 +97,46 @@ function _G.Main:New(Title)
 		TextButton.TextScaled = true
 		TextButton.TextSize = 14.000
 		TextButton.TextWrapped = true
-
-
+			
+			
 		TextButton.MouseButton1Click:Connect(function()
 			pcall(Call)
+			wait(0.1)
+			pcall(nil)
 		end)
+			
+			function _G.Frame:Notify(Name,Call)
+				local notify = Instance.new("Frame")
+				notify.BackgroundColor3 = Color3.new(0, 0.176471, 0.666667)
+				notify.Position = UDim2.new(1, 0, 0.293133467, 0)
+				notify.Size = UDim2.new(0.27136752, 0, 0.346805751, 0)
+				notify.Name = "Notify"
+				notify.Parent = Frame
+				local uicorner = Instance.new("UICorner")
+				uicorner.Parent = notify
+
+				local notify_text = Instance.new("TextLabel")
+				notify_text.Font = Enum.Font.SourceSans
+				notify_text.Text = Name
+				notify_text.TextColor3 = Color3.new(0, 0, 0)
+				notify_text.TextScaled = true
+				notify_text.TextSize = 14
+				notify_text.TextWrapped = true
+				notify_text.BackgroundColor3 = Color3.new(1, 1, 1)
+				notify_text.BackgroundTransparency = 1
+				notify_text.Size = UDim2.new(1, 0, 1, 0)
+				notify_text.Name = "NotifyText"
+				notify_text.Parent = notify
+				while true do
+					wait(13)
+					notify:Destroy()
+				end
+			end
+		
 		function _G.Frame:ToggleButton(Name,Call)
 			local Toggle = Instance.new("TextButton")
 			Toggle.Name = "Toggle"
-			Toggle.Parent = Part
+			Toggle.Parent = part
 			Toggle.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
 			Toggle.Position = UDim2.new(0, 0, 1.46395189e-08, 0)
 			Toggle.Size = UDim2.new(0.139093146, 0, 0.228410542, 0)
@@ -125,21 +145,42 @@ function _G.Main:New(Title)
 			Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
 			Toggle.TextScaled = true
 			Toggle.TextSize = 14.000
-			Toggle.TextWrapped = true
-			local On = Instance.new("BoolValue")
-			On.Name = "On"
-			On.Parent = Toggle
+				Toggle.TextWrapped = true
+				local On = Instance.new("BoolValue")
+				On.Name = "On"
+				On.Parent = Toggle
+				local image_label = Instance.new("ImageLabel")
+				image_label.Image = "http://www.roblox.com/asset/?id=168367448"
+				image_label.BackgroundColor3 = Color3.new(255, 0, 0)
+				image_label.BorderColor3 = Color3.new(255, 0, 0)
+				image_label.Position = UDim2.new(0.789589942, 0, 0.91027236, 0)
+				image_label.Size = UDim2.new(0.35845834, 0, 0.257133663, 0)
+				image_label.Parent = workspace
+
+				local uicorner = Instance.new("UICorner")
+				uicorner.Parent = Toggle
 			Toggle.MouseButton1Click:Connect(function()
 				while true do
 					wait(0.1)
-					if Toggle.On.Value == false then
-						wait(0.1)
-						Toggle.On.Value = true
-						pcall(Call)
-					else
-						wait(0.1)
-						Toggle.On.Value = false
-					end
+						if On.Value == false then
+							wait(0.1)
+						On.Value = true
+						else
+							wait(0.1)
+						On.Value = false
+						end
+						while true do
+							wait(0.1)
+							if On.Value == true then
+								
+									wait(0.3)
+									pcall(Call)
+							else
+								pcall(nil)
+
+							end
+						end
+	
 				end
 			end)
 
@@ -147,5 +188,4 @@ function _G.Main:New(Title)
 	end
 
 	return _G.Frame
-
 end
